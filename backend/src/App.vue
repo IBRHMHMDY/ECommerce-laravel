b4c5956bb273ca7a271862c52927ad8238092850
IBRHMHMDY <ibrhmhmdy@gmail.com>
2025-04-19 12:30:48 +0200

    Fix Tailwindcss with Vue

--- backend/src/App.vue
+++ backend/src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
-
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-    <h1 class="text-2xl text-blue-500">Welcome Tailwindcss with Vue3</h1>
+    <h1 class="flex items-center justify-center m-10 text-center text-2xl text-blue-500 font-bold">Hello Vite + TailwindCSS + Vue3</h1>
 </template>
 
 <style scoped>
-
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
